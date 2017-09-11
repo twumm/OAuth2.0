e2676f45c87651e140f6e6f18daf1ba03609344b
twumm <martint.mensah@gmail.com>
2017-09-11 21:13:12 +0000

    Add user table

--- database_setup.py
+++ database_setup.py
@@ -5,11 +5,21 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -22,14 +32,15 @@ class Restaurant(Base):
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -45,7 +56,8 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+# engine = create_engine('sqlite:///restaurantmenu.db')
  
 
 Base.metadata.create_all(engine)
